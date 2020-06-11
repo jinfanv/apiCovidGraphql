9fcae9928cd592f2ef88c65413696434f678520a
Jonattan Infante Vargas <infanpearl@gmail.com>
2020-06-11 12:54:47 -0500

    Correción de configuraciones de Servidor en relación a las variables

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-'use strict'
+"use strict";
 
-require('dotenv').config()
-const {makeExecutableSchema } = require('graphql-tools')
+require("dotenv").config();
+const { makeExecutableSchema } = require("graphql-tools");
 
-const {readFileSync} = require('fs')
-const {join} = require('path')
+const { readFileSync } = require("fs");
+const { join } = require("path");
 
-const resolvers = require('./Lib/resolvers')
+const resolvers = require("./Lib/resolvers");
 
-const cors = require('cors')
+const cors = require("cors");
 
-const express = require('express')
-const gqlMiddleware = require('express-graphql')
-const app = express()
+const express = require("express");
+const gqlMiddleware = require("express-graphql");
+const app = express();
 
-const port = process.env.PORT || 3000
-const isDev = process.env.NODE_ENV == "production"?false:true
+const port = process.env.PORT || 3000;
+const isDev = process.env.NODE_ENV == "production" ? false : true;
 
-const typeDefs = readFileSync(
-    join(__dirname,'Lib','shema.graphql'),
-    'utf-8'
-)
+const typeDefs = readFileSync(join(__dirname, "Lib", "shema.graphql"), "utf-8");
 
 // schema
-const schema = makeExecutableSchema({typeDefs,resolvers})
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-app.use(cors())
+app.use(cors());
 
-app.use('/', gqlMiddleware({
-  schema: schema,
-  rootValue: resolvers,
-  graphiql: isDev
-}))
+app.use(
+  "/",
+  gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: isDev,
+  })
+);
 
 app.listen(port, () => {
-  console.log(`Server is Listening at http://localhost:${port}/bed`)
-})
+  console.log(`Server is Listening at http://localhost:${port}`);
+});
